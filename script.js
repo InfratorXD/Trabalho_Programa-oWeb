171925af8c1c0df1d92fc60ad5017c784f011bf9
bruno.hsl <bruno.hsl.077@gmail.com>
2024-09-25 22:47:54 -0300

    formulario

---
+++ script.js
@@ -0,0 +1,6 @@
+// função para esconder a div inicial
+
+function comecar(){
+    document.getElementById("paginaInicial").style.display = "none";
+    document.getElementById("formulario").style.display = "block";
+}
