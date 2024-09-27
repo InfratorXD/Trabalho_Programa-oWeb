15af7275007ac22652903a0bece3477f9882340a
InfratorXD <guiguicazeee@gmail.com>
2024-09-26 22:27:37 -0300

    correções

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+
 // função para esconder a div inicial
 
 function comecar(){
-    document.getElementById("paginaInicial").style.display = "none";
+document.getElementById("paginaInicial").style.display = "none";
+document.getElementById("formulario").style.display = "block";
+document.getElementById("startButton").addEventListener("click", function() {
+    // Mostra o container do quiz
     document.getElementById("formulario").style.display = "block";
+    
+    // Rola a página para as perguntas
+    document.getElementById("formulario").scrollIntoView({ behavior: "smooth" });
+});
+
 }
